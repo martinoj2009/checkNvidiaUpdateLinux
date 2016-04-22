ca42b3bedf30be5b1c0805a913e9b2e1dbb14793
Martino <martinojones@outlook.com>
2016-04-22 15:41:25 -0400

    Fixed the whole thing. Now gets the current and latest version

--- check.py
+++ check.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import socket
-import subprocess
+import urllib.request
 import os
 
 '''
 Author: Martino Jones
-Description: This is a Python3 script that will check Nvidia servers for
-    am update for your Nvidia GPU.
+Description: This is a Python3 script that will check Nvidia servers for am update for your Nvidia GPU.
 '''
 
 #Check to see if the server is available
@@ -23,8 +22,23 @@ def is_connected():
 def getGPU():
     output = os.popen('lspci | grep -i vga | grep -i nvidia').read()
     output = output.split('[')[1].split(']')[0]
-    print(output)
+    return output
 
+def getCurrentVersion():
+    output = os.popen('nvidia-smi | grep Version').read()
+    output = output.split("Version:")[1].split('|')[0].strip(" ")
+    return output
+
+def getLatestVersion():
+    #This will get the latest version by checking the site
+    versions = []
+    u2 = urllib.request.urlopen('http://www.nvidia.com/object/linux-amd64-display-archive.html')
+
+    for lines in u2.readlines():
+        if "Version" in lines.decode("utf-8"):
+            versions.append(lines.decode("utf-8"))
+
+    return versions[0].split("Version:")[1].split("<br>")[0].strip(" ")
 
 
 def main():
@@ -33,8 +47,15 @@ def main():
         print("Cannot connect to Nvidia server. Can't continue.")
         exit()
 
-    getGPU()
+    GPU = getGPU()
+    latestVerion = getLatestVersion()
+    currentVersion = getCurrentVersion()
 
+    print("GPU: " + getGPU())
+    if float(latestVerion) > float(currentVersion):
+        print("Update available\n")
+        print("You're on: " + currentVersion)
+        print("Latest version: " + latestVerion)
 
 
 
